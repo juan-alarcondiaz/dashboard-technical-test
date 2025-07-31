836e4996c868e858b3073b0bec1a8187b94ce493
Juan Alarcon <juan.s.alarcondiaz@gmail.com>
2025-07-30 19:14:38 -0500

    Add MinimalLayout component with template and styles.
    - Created `MinimalLayout` component with structure in `minimal-layout.html` and styles in `minimal-layout.scss`.
    - Added basic template to display component functionality.

---
+++ src/app/core/layouts/minimal-layout/minimal-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'erp-minimal-layout',
+  imports: [],
+  templateUrl: './minimal-layout.html',
+  styleUrl: './minimal-layout.scss'
+})
+export class MinimalLayout {
+
+}
