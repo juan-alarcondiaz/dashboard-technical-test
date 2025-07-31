2c03b217094f9c469137eafe5ea226d7abcb2a12
Juan Alarcon <juan.s.alarcondiaz@gmail.com>
2025-07-30 19:28:23 -0500

    Set up application routes with lazy-loaded components and layouts
    - Added default redirect to `/dashboard`.
    - Configured `MainLayout` with `Dashboard` as a child route.
    - Configured `MinimalLayout` with fallback `NotFound` route.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  {
+    path: '',
+    loadComponent: () => import('./core/layouts/main-layout/main-layout').then(m => m.MainLayout),
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./dashboard/dashboard').then(m => m.Dashboard)
+      }
+    ]
+  },
+  {
+    path: '',
+    loadComponent: () => import('./core/layouts/minimal-layout/minimal-layout').then(m => m.MinimalLayout),
+    children: [
+      { path: '**', loadComponent: () => import('./not-found/not-found').then(m => m.NotFound) },
+    ]
+  }
+];
