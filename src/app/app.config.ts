b20d73c9eb5aaf0a10c8fd63f585fecf5af61d7a
Juan Alarcon <juan.s.alarcondiaz@gmail.com>
2025-07-30 19:05:06 -0500

    Add global providers for HttpClient, NgIcons, and Charts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,21 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideNgIconsConfig } from '@ng-icons/core';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(
+      withFetch()
+    ),
+    provideCharts(withDefaultRegisterables()),
+    provideNgIconsConfig({
+      size: '1.375em',
+    }), provideCharts(withDefaultRegisterables()),
   ]
 };
