bdbf5f91ea49a34567b6a77b9616ac2550fbe4dc
Juan Alarcon <juan.s.alarcondiaz@gmail.com>
2025-07-30 19:15:22 -0500

    Add Input component with layout, and styles
    - Created `Input` component with structure in `input.html` and styles in `input.scss`.
    - Added basic template to support `input` and `label` elements using `ng-content`.

---
+++ src/app/shared/components/input/input.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'erp-input',
+  imports: [],
+  templateUrl: './input.html',
+  styleUrl: './input.scss'
+})
+export class Input {
+
+}
